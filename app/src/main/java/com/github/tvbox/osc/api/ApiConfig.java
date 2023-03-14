282b9f82242e920d86851871b90dbeb4a9e039c2
kaigehulian <127667184+kaigehulian@users.noreply.github.com>
2023-03-14 18:57:08 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://xn--4bra.live/%E7%8C%AB");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
